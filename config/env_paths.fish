b2155e1fc8fd44eafaa898bd61efdf83d642b18a
Yuto Tanaka <git@mimikun.dev>
2026-05-05 14:46:54 +0900

    feat: add some envs

--- config/env_paths.fish
+++ config/env_paths.fish
@@ -314,6 +314,15 @@ set -gx PATH "$HOME/.moon/bin" $PATH
 # Browser-Use
 set -gx PATH "$HOME/.browser-use-env/bin" $PATH
 
+# https://donottrack.sh
+set -x DO_NOT_TRACK 1
+
+# https://force-color.org
+set -x FORCE_COLOR 1
+
+# https://no-color.org
+set -x NO_COLOR 0
+
 # age public keys
 set -gx AGE_PUBKEY_HOME "age1cktdwc8u4z76kawluyswaruyeg7eg6078rw3t0kkmx5wqkk40d2qzajn8y"
 set -gx AGE_PUBKEY_WORK "age1n03v0casqk2djs2jw3xq5ldpdjtp2s6r0u3uhtmd7zv9j2xuyf6qpl9x7l"
