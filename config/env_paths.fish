61bc4f27e01524e90970f07262280ff93a0f1390
Yuto Tanaka <git@mimikun.dev>
2026-06-21 15:19:59 +0900

    feat: add git-ai path

--- config/env_paths.fish
+++ config/env_paths.fish
@@ -323,6 +323,9 @@ set -gx PATH "$HOME/.browser-use-env/bin" $PATH
 # https://no-color.org
 #set -x NO_COLOR 0
 
+# Added by git-ai installer on Fri May 29 03:18:44 PM JST 2026
+set -gx PATH "$HOME/.git-ai/bin" $PATH
+
 # age public keys
 set -gx AGE_PUBKEY_HOME "age1cktdwc8u4z76kawluyswaruyeg7eg6078rw3t0kkmx5wqkk40d2qzajn8y"
 set -gx AGE_PUBKEY_WORK "age1n03v0casqk2djs2jw3xq5ldpdjtp2s6r0u3uhtmd7zv9j2xuyf6qpl9x7l"
