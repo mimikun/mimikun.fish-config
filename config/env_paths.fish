fb99a96b4a64146981e75cbe47db472c2b2284d0
Yuto Tanaka <git@mimikun.dev>
2026-07-02 21:47:12 +0900

    feat(env): add simutil PATH

--- config/env_paths.fish
+++ config/env_paths.fish
@@ -327,6 +327,9 @@ set -gx PATH "$HOME/.browser-use-env/bin" $PATH
 # Added by git-ai installer on Fri May 29 03:18:44 PM JST 2026
 set -gx PATH "$HOME/.git-ai/bin" $PATH
 
+# simutil
+set -gx PATH "$HOME/.local/lib/simutil" $PATH
+
 # age public keys
 set -gx AGE_PUBKEY_HOME "age1cktdwc8u4z76kawluyswaruyeg7eg6078rw3t0kkmx5wqkk40d2qzajn8y"
 set -gx AGE_PUBKEY_WORK "age1n03v0casqk2djs2jw3xq5ldpdjtp2s6r0u3uhtmd7zv9j2xuyf6qpl9x7l"
